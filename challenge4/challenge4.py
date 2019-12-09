35219c2462757658de8a99c8c97d96438a7b9680
nathaniel.trainor@sling.com <nathaniel.trainor@sling.com>
2019-12-09 16:47:01 -0700

    Fixed challenge 3 to be more durable

---
+++ challenge4/challenge4.py
@@ -0,0 +1,25 @@
+import unittest
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class Challenge4(unittest.TestCase):
+
+    def setUp(self):
+        # code to startup webdriver
+        self.driver = webdriver.Chrome("../chromedriver.exe")
+
+    def tearDown(self):
+        # code to close webdriver
+        self.driver.close()
+
+    def test_challenge4(self):
+        # code for our test steps
+
+
+    if __name__ == '__main__':
+        unittest.main()
