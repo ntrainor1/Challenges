231a6f572a55b3421b48054dbc9f41ef4bffb952
nathaniel.trainor@sling.com <nathaniel.trainor@sling.com>
2019-12-18 13:56:50 -0700

    Fixed challenge 6

--- challenge6/challenge6.py
+++ challenge6/challenge6.py
@@ -27,6 +27,7 @@ class Challenge6(unittest.TestCase):
         # Once the search page is loaded, search for "skyline" in the secondary searchbar
         WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, "//*[@placeholder=\"Search by Lot#,VIN#\"]")))
         # self.driver.find_element(By.XPATH, "//*[@placeholder=\"Search by Lot#,VIN#\"]").send_keys("skyline")
+        # time.sleep(10)
 
         # Try to find an entry with "skyline", otherwise take a screenshot
         try:
